df0d3587a0a7d9da8bc57437e419796230daf2ce
khalatesonalilive <khalatesonali1234@gmail.com>
2024-07-17 13:54:45 +0530

    PIMPage Test Cases by Adyant Tester

---
+++ src/main/java/Tutorial2/Demo2Git.java
@@ -0,0 +1,11 @@
+package Tutorial2;
+
+public class Demo2Git {
+
+	public static void main(String[] args) {
+		
+		System.out.println("PIMPage Test Cases written by the Adyant Automation Tester");
+
+	}
+
+}
